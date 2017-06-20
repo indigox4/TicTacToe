c2b51df672715ad35e9e7ff3ff227cf1d3eb0886
Stephen Yee <indigox3@hotmail.com>
2017-06-19 21:27:47 -0400

    Try another way of listening to the port

--- Server/index.js
+++ Server/index.js
@@ -369,9 +369,15 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen( process.env.PORT || 3000, function(){
-  console.log('listening on *: some port tbd');
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
+// http.listen( process.env.PORT || 3000, function(){
+//   console.log('listening on *: some port tbd');
+// });
 
 
 
